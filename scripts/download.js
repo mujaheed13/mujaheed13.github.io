a157b6142f90ff5c79ebce8ec496fa24799a0eb3
unknown <mujaheedsami13@gmail.com>
2023-02-02 23:24:23 +0530

    Stated working on Home & About

---
+++ scripts/download.js
@@ -0,0 +1,7 @@
+const resume_btn = document.querySelector(".resume");
+resume_btn.addEventListener("click", ()=>{
+    window.Location.href = "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link"
+    console.log(11);
+})
+
+console.log(resume_btn);     
---
+++ scripts/home.js
@@ -0,0 +1,49 @@
+var words = ["Web Developer", "Coder", "Programmer", "Backend Developer"]
+
+
+var counter = 0;
+var currentIndex = getRandomInt(0, words.length - 1);
+
+var text = document.getElementById("text2");
+
+var stepInterval = setInterval(() => { step(); }, 200);
+var delInterval = null;
+var delTimeout = null;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function delIntervalCallback(){
+    delInterval = setInterval(() => { del(); }, 100);
+    clearTimeout(delTimeout);
+}
+
+function del(){
+    if(counter == 0){
+        let newIndex = getRandomInt(0, words.length - 1);
+        while(newIndex == currentIndex){
+            newIndex = getRandomInt(0, words.length -1 );
+        }
+        currentIndex = newIndex;
+        clearInterval(delInterval);
+        stepInterval = setInterval(() => { step(); }, 200);
+    }
+    else{
+        text.textContent = text.textContent.slice(0, -1)
+        counter--;
+    }
+}
+
+function step(){
+    if(counter >= words[currentIndex].length){
+        clearInterval(stepInterval);
+        delTimeout = setTimeout(() => { delIntervalCallback(); }, 2000);
+    }
+    else{
+        text.textContent += words[currentIndex][counter];
+        counter++;
+    }
+}
