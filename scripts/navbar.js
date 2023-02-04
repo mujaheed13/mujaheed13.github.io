db70aee8bca72e929437ff88457735a61689a822
unknown <mujaheedsami13@gmail.com>
2023-02-04 11:14:26 +0530

    Updated navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,11 @@
 const navBtn = document.getElementById("nav-btn");
-const navMenue = document.getElementById("nav-menu");
+const navMenu = document.getElementById("nav-menu");
 let navActive = false;
 navBtn.addEventListener("click", ()=>{
     if(!navActive){
         navBtn.src = "./images/cross.png"
         navActive=true;
-        navMenue.style = `
+        navMenu.style = `
             display: flex;
             background-color: #ffffff;
             width: 100vw;
@@ -17,6 +17,6 @@ navBtn.addEventListener("click", ()=>{
     } else {
         navBtn.src = "./images/menu-burger.png"
         navActive=false;
-        navMenue.style.display = "none";
+        navMenu.style.display = "none";
     }
 })
