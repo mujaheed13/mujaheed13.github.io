7825b1e6f1edb6cac96726ee95cbffd6887ef510
unknown <mujaheedsami13@gmail.com>
2023-02-08 00:21:18 +0530

    Completed Home Section

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,23 +1,32 @@
 const navBtn = document.getElementById("nav-btn");
 const navMenu = document.getElementById("nav-menu");
+const nav_btns = document.querySelectorAll(".nav-link");
 let navActive = false;
 navBtn.addEventListener("click", ()=>{
     if(!navActive){
-        navBtn.src = "./images/cross.png"
+        navBtn.src = "./images/cross.png";
         navActive=true;
         navMenu.style = `
             display: flex;
-            background-color: #ffffff;
+            // background-color: #ffffff;
             width: 100vw;
-            position: absolute;
+            position: fixed;
             box-shadow: none;
             margin: 0;
             border-radius:0;
             transition: 1s;
         `;
     } else {
-        navBtn.src = "./images/menu-burger.png"
+        navBtn.src = "./images/menu-burger.png";
         navActive=false;
         navMenu.style.display = "none";
     }
+})
+
+nav_btns.forEach((el)=>{
+    el.addEventListener("click", ()=>{
+        navBtn.src = "./images/menu-burger.png";
+        navActive=false;
+        navMenu.style.display = "none";  
+    })
 })
