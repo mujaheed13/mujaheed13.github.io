8973cf92324b42785bf90cac764a83d660a37f62
unknown <mujaheedsami13@gmail.com>
2023-02-13 22:01:31 +0530

    added GitHub Calendar

--- scripts/download.js
+++ scripts/download.js
@@ -1,4 +1,4 @@
-const resume_btn = document.querySelector("#resume-button-1");
+const resume_btn = document.querySelector("#resume-link-1");
 
 resume_btn.addEventListener("click", ()=>{
     window.open("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
