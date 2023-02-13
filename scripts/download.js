9d9964ff884b519741c683460e026fbfff0c2f66
unknown <mujaheedsami13@gmail.com>
2023-02-13 22:16:44 +0530

    GitHub Stats Styling fixed

--- scripts/download.js
+++ scripts/download.js
@@ -1,6 +1,9 @@
-const resume_btn = document.querySelector("#resume-link-1");
+const resume_btn1 = document.querySelector("#resume-link-1");
+const resume_btn2 = document.querySelector("#resume-link-2");
 
-resume_btn.addEventListener("click", ()=>{
+resume_btn1.addEventListener("click", download);   
+resume_btn2.addEventListener("click", download);
+
+function download(){
     window.open("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
-})     
-
+}
