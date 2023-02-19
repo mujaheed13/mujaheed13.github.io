b3c80e58cdbac1ba89a589fe881c670ed5710bef
unknown <mujaheedsami13@gmail.com>
2023-02-19 11:23:29 +0530

    updated skills

--- scripts/download.js
+++ scripts/download.js
@@ -1,9 +1,12 @@
 const resume_btn1 = document.querySelector("#resume-link-1");
 const resume_btn2 = document.querySelector("#resume-link-2");
 
-resume_btn1.addEventListener("click", download);   
+resume_btn1.addEventListener("click", download);
 resume_btn2.addEventListener("click", download);
 
-function download(){
-    window.open("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
+function download() {
+  window.open(
+    "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link",
+    "_blank"
+  );
 }
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,11 +2,11 @@ const navBtn = document.getElementById("nav-btn");
 const navMenu = document.getElementById("nav-menu");
 const nav_btns = document.querySelectorAll(".nav-link");
 let navActive = false;
-navBtn.addEventListener("click", ()=>{
-    if(!navActive){
-        navBtn.src = "./images/cross.png";
-        navActive=true;
-        navMenu.style = `
+navBtn.addEventListener("click", () => {
+  if (!navActive) {
+    navBtn.src = "./images/cross.png";
+    navActive = true;
+    navMenu.style = `
             display: flex;
             width: 100vw;
             position: fixed;
@@ -17,19 +17,19 @@ navBtn.addEventListener("click", ()=>{
                 -webkit-animation: slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
                         animation: slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
         `;
-    } else {
-        navBtn.src = "./images/menu-burger.png";
-        navActive=false;
-        navMenu.style.display = "none";
-    }
-})
+  } else {
+    navBtn.src = "./images/menu-burger.png";
+    navActive = false;
+    navMenu.style.display = "none";
+  }
+});
 
-nav_btns.forEach((el)=>{
-    el.addEventListener("click", ()=>{
-        navBtn.src = "./images/menu-burger.png";
-        if(window.innerWidth<=768){
-            navActive=false;
-            navMenu.style.display = "none"; 
-        } 
-    })
-})
+nav_btns.forEach((el) => {
+  el.addEventListener("click", () => {
+    navBtn.src = "./images/menu-burger.png";
+    if (window.innerWidth <= 768) {
+      navActive = false;
+      navMenu.style.display = "none";
+    }
+  });
+});
