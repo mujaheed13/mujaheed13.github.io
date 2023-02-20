92f1e5d3380463b2a642f41ae101235aa98912b8
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-02-20 22:38:24 +0530

    updated resume btn

--- scripts/download.js
+++ scripts/download.js
@@ -1,5 +1,5 @@
-// const resume_btn1 = document.querySelector("#resume-link-1");
-// const resume_btn2 = document.querySelector("#resume-link-2");
+const resume_btn1 = document.querySelector("#resume-link-1");
+const resume_btn2 = document.querySelector("#resume-link-2");
 
 // resume_btn1.addEventListener("click", download);
 // resume_btn2.addEventListener("click", download);
@@ -11,10 +11,11 @@
 //   );
 // }
 
-document.getElementById('resume-link-1').addEventListener("click",()=>{
+resume_btn2.addEventListener("click",()=>{
   window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
 })
 
-document.getElementById('resume-link-2').addEventListener("click",()=>{
+document.getElementById('resume-link-1').addEventListener("click",()=>{
   window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
+  
 })
