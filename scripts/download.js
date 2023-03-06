192d28cd13b0fe45b84fd18521420022a3f5437e
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-06 22:21:05 +0530

    Updated Resume Button

--- scripts/download.js
+++ scripts/download.js
@@ -4,18 +4,18 @@ const resume_btn2 = document.querySelector("#resume-link-2");
 resume_btn1.addEventListener("click", download);
 resume_btn2.addEventListener("click", download);
 
-function download() {
-  window.open(
-    "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link",
-    "_blank"
-  );
-}
+// function download() {
+//   window.open(
+//     "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link",
+//     "_blank"
+//   );
+// }
 
-// resume_btn2.addEventListener("click",()=>{
-//   window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
-// })
+resume_btn2.addEventListener("click",()=>{
+  window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
+})
 
-// document.getElementById('resume-link-1').addEventListener("click",()=>{
-//   window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
+document.getElementById('resume-link-1').addEventListener("click",()=>{
+  window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
   
-// })
+})
