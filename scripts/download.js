865ab117968a396642d43482d5c9abfdbd51e304
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-06 22:27:37 +0530

    Fixed error

--- scripts/download.js
+++ scripts/download.js
@@ -1,8 +1,8 @@
 const resume_btn1 = document.querySelector("#resume-link-1");
 const resume_btn2 = document.querySelector("#resume-link-2");
 
-resume_btn1.addEventListener("click", download);
-resume_btn2.addEventListener("click", download);
+// resume_btn1.addEventListener("click", download);
+// resume_btn2.addEventListener("click", download);
 
 // function download() {
 //   window.open(
@@ -12,10 +12,10 @@ resume_btn2.addEventListener("click", download);
 // }
 
 resume_btn2.addEventListener("click",()=>{
-  window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link"  );
+  window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
 })
 
 document.getElementById('resume-link-1').addEventListener("click",()=>{
-  window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link");
+  window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
   
 })
