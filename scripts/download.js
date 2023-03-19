3842db1a864607bb5933135c310ad80767e88aa4
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-19 23:31:57 +0530

    Updated resume

--- scripts/download.js
+++ scripts/download.js
@@ -1,5 +1,5 @@
-const resume_btn1 = document.querySelector("#resume-link-1");
-const resume_btn2 = document.querySelector("#resume-link-2");
+const resume_btn1 = document.querySelector("#resume-button-1");
+const resume_btn2 = document.querySelector("#resume-button-2");
 
 // resume_btn1.addEventListener("click", download);
 // resume_btn2.addEventListener("click", download);
@@ -20,5 +20,8 @@ resume_btn2.addEventListener("click", download);
 
 function download(){
   window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
-  
+  // window.open(
+  //       "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link",
+  //       "_blank"
+  //     );
 }
